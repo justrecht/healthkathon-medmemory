0c6d149c9e54f5a7e7f17d4055780ce51e83d81b
justrecht <ikadektresnajeverly78@gmail.com>
2025-11-18 23:47:22 +0800

    feat: add Firebase SDK and configuration
    - Added firebase package to package.json
    - Created firebase configuration file to initialize Firebase app

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9GEeHB91pflX8l4A1jyFa60-zExfaDJo",
+  authDomain: "healthkathon-medmemory.firebaseapp.com",
+  projectId: "healthkathon-medmemory",
+  storageBucket: "healthkathon-medmemory.firebasestorage.app",
+  messagingSenderId: "696161324408",
+  appId: "1:696161324408:web:0433668b80bf164de84c8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
